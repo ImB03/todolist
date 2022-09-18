a4f8bf1a684acd426692ab4de264d547d6823599
ImB03 <giabao7803@gmail.com>
2022-09-18 17:27:27 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -8,13 +8,25 @@ export default function App() {
     setJobs((prev) => [...prev, job]);
     setJob("");
   };
+
+  const handleDelete = (index) => {
+    const newJobs = [...jobs];
+    newJobs.splice(index, 1);
+    setJobs(newJobs);
+    
+  };
   return (
     <div style={{ padding: 32 }}>
       <input value={job} onChange={(e) => setJob(e.target.value)} />
       <button onClick={handleSubmit}>Add</button>
       <ul>
         {jobs.map((job, index) => {
-          return <li key={index}>{job}</li>;
+          return (
+            
+              <li key={index}>{job} <button value={index} onClick={()=>handleDelete(index)}>Delete</button></li>
+              
+            
+          );
         })}
       </ul>
     </div>
